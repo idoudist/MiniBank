0a38bf7acaf871801338344dfea1a9a070582b37
Fadi Idoudi <fadi.idoudi@outlook.com>
2023-10-12 19:19:42 +0200

    remove unused files

--- Front/karma.conf.js
+++ Front/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/JoodLab'),
+      dir: require('path').join(__dirname, './coverage/MiniBank'),
       subdir: '.',
       reporters: [
         { type: 'html' },
