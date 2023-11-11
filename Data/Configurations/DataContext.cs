26ed1c75b93e12a5a33c8e52106b7cc37b5123fc
Fadi Idoudi <fadi.idoudi@outlook.com>
2023-11-11 18:28:10 +0100

    feat(Domain): add identity domain and migrations

--- Data/Configurations/DataContext.cs
+++ Data/Configurations/DataContext.cs
@@ -1,6 +1,11 @@
-﻿namespace Data.Configurations;
+﻿using Domain.Entities.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
-public class DataContext : DbContext
+namespace Data.Configurations;
+
+public class DataContext : IdentityDbContext<AppUser, AppRole, int, IdentityUserClaim<int>, AppUserRole,
+        IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
 {
     public DataContext(DbContextOptions options) : base(options)
     {
@@ -12,6 +17,21 @@ public class DataContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        
+        base.OnModelCreating(builder);
+
+        /*configure identity relations*/
+        // configure relation user - userRole
+        builder.Entity<AppUser>()
+            .HasMany(u => u.UserRoles)
+            .WithOne(ur => ur.User)
+            .HasForeignKey(ur => ur.UserId)
+            .IsRequired();
+        // configure relation roke - userRole
+        builder.Entity<AppRole>()
+            .HasMany(u => u.UserRoles)
+            .WithOne(ur => ur.Role)
+            .HasForeignKey(ur => ur.RoleId)
+            .IsRequired();
+
     }
 }
---
+++ Domain/Entities/Identity/AppUserRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities.Identity;
+
+public class AppUserRole : IdentityUserRole<int>
+{
+    public AppUser User { get; set; }
+    public AppRole Role { get; set; }
+}
